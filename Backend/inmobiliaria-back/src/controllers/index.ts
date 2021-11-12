aca9ce2b44167da41ff8b62c6326b6e54ebef8c9
Daniela Robles Urbina <danisru02@hotmail.com>
2021-11-12 11:37:46 -0500

    Controladores de microservicio-CRUD

--- Backend/inmobiliaria-back/src/controllers/index.ts
+++ Backend/inmobiliaria-back/src/controllers/index.ts
@@ -2,3 +2,7 @@ export * from './ping.controller';
 export * from './imagen-inmueble.controller';
 export * from './zona-inmueble.controller';
 export * from './inmueble-imagen.controller';
+export * from './inmueble.controller';
+export * from './usuario.controller';
+export * from './imagen.controller';
+export * from './zona.controller';
