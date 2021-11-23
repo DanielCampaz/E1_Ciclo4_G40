36e08f519bfb45f3f27b6e8f015cf3457c8592c8
Daniel Steven Campaz OCoro <dscampaz3110@gmail.com>
2021-11-22 21:55:17 -0500

    Añadiendo y Arreglando el Front y entrutammientos

--- Frontend/inmobiliaria-front/src/app/app-routing.module.ts
+++ Frontend/inmobiliaria-front/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/security/login/login.component';
 import { SignUpComponent } from './modules/security/sign-up/sign-up.component';
 import { HomeComponent } from './public/general/home/home.component';
+import { NotFoundComponent } from './public/errors/not-found/not-found.component';
+import { ResetPasswordComponent } from './modules/security/reset-password/reset-password.component';
 
 const routes: Routes = [
   {
@@ -16,11 +18,19 @@ const routes: Routes = [
   },
   {
     path: "singup",
-    component: SignUpComponent
+    component: SignUpComponent 
   },
   {
     path: "login",
     component: LoginComponent
+  },
+  {
+    path: "resetPasword",
+    component: ResetPasswordComponent
+  },
+  {
+    path:"**",
+    component: NotFoundComponent
   }
 ];
 
